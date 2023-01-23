7c72bd7b2842bae128bcddb95e60fe5e0a0c9b27
kodegosp404 <kodegosp404@gmail.com>
2023-01-23 11:07:55 +0800

    PHP Lectures

--- phpdemo/02_variables.php
+++ phpdemo/02_variables.php
@@ -20,3 +20,52 @@
 - variables can't start with a number
 - Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 - Variables are case-sensitive ($name and $NAME are two different variables)*/
+
+
+$fname = 'Ryan'; //String can be single or double quotes
+$age = 41; //integer
+$hasKids = False; //Boolean
+$cashOnHand = 10; //float
+
+// echo "$fname is $age years old";
+// echo $hasKids;
+// var_dump($hasKids);
+// echo $cashOnHand;
+// var_dump($cashOnHand);
+
+//This will not work
+// echo '$fname is $age years old';
+
+//concatenate strings
+// echo "$fname is $age years old";
+// echo "<br>";
+echo $fname . ' is ' . $age . ' years old'; 
+echo nl2br("\n$fname is $age years old");
+
+//Arithmetic Operators
+echo "<br>";
+
+echo '5' + '5';
+$x = '5' + '5';
+echo "<br>";
+echo $x;
+echo "<br>";
+var_dump($x);
+echo "<br>";
+echo 5 + 5;
+
+echo "<br>";
+echo 10 -6;
+
+echo "<br>";
+echo 95 * 95;
+
+echo "<br>";
+echo 500 / 25;
+
+//Constants - Cannot be changed
+echo "<br>";
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+var_dump(HOST);
